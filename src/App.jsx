98110a0df82554983c4cb47af94fcd85d0b70a4c
Raúl <raul.rodriguez.c.2012@gmail.com>
2024-11-13 00:46:53 -0300

    Carga de archivos 1er Hito

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import "./index.css"
+import {Navbar} from "./components/Navbar"; 
+import Home from "./components/Home.jsx";
+import {Footer} from "./components/Footer.jsx";
 
 function App() {
-
+  
 
   return (
     <>
+      <Navbar/>
+      <Home/>
+      <Footer/>
     </>
   )
 }
 
 export default App
+
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./Header";
+import { PizzaCards } from "./CardPizza";
+
+const Home = () => {
+    return (
+        <>
+        <Header/>
+        <PizzaCards/>
+        </>
+    
+    );
+};
+
+export default Home;
